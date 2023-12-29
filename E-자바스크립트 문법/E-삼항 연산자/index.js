7c8b2e3af23d8d7cd1a335760ac835c732c7c656
lsj0202 <w1004123@gmail.com>
2023-12-29 09:06:01 +0900

    E등급 삼항 연산자 처치

---
+++ E-자바스크립트 문법/E-삼항 연산자/index.js
@@ -0,0 +1,17 @@
+let a = 3;
+
+if (a >= 0) {
+  console.log("양수");
+} else {
+  console.log("음수");
+}
+
+a >= 0 ? console.log("양수") : console.log("음수");
+
+let grade = 100;
+
+score >= 90
+  ? console.log("A")
+  : score >= 80
+  ? console.log("B")
+  : console.log("C");
